7f495ea75b198ab81d773abf0967aee3e6de7df3
sutiwatNG <sutiwatngampiam@gmail.com>
2024-02-02 19:47:30 +0700

    update

--- Variable.java
+++ Variable.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 class Variable {
     public static void main(String[] args) {
-        final int num=10;
-        System.out.println(num);
+        Scanner kb =new Scanner(System.in);
+        System.out.print("money : ");
+        int money=kb.nextInt();
+        System.out.println("count of money ="+money);
+        
+        if (money>=1000) {
+            System.out.println("1000 = "+(money/1000)+" Bai");
+            money%=1000;
+        }
+        if (money>=500) {
+            System.out.println("500 = "+(money/500)+" Bai");
+            money%=500;
+        }
     }
 }
